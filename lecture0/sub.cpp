372b9f2596a3ba1d9663ffd2c87801d16ef3856c
kashishsrivas <kashishsrivastava.delhi@gmail.com>
2024-08-28 14:39:23 +0530

    message

---
+++ lecture0/sub.cpp
@@ -0,0 +1,13 @@
+//this is a subtraction program
+//it will subtract two numbers and print the result
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b, sub;
+    a= 12;
+    b = 10;
+    sub = a - b;
+    cout<< sub;
+    return 0;
+}
